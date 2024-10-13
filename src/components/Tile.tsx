22f1b1a83d3da1ecb788c53d8dc2ad59a82d8d35
Michal Witkowski <WitkowskiMichau@gmail.com>
2024-10-13 22:33:34 +0100

    work in progress #6

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,7 +2,7 @@ import { TileProps } from "@/components/Types";
 
 const Tile = ({ title, description, icon }: TileProps) => {
     return (
-        <div className="bg-black rounded-lg p-6 w-72 h-56 border-primaryYellow border-2 border-opacity-25 transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-102 hover:shadow-lg hover:border-gradient-to-r hover:from-pink-500 hover:to-blue-500 cursor-pointer">
+        <div className="bg-black rounded-lg p-6 w-72 h-56 border-primaryYellow border-2 border-opacity-25 transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-102 hover:shadow-bright hover:border-shine cursor-pointer">
             <div className="flex items-center mb-4">
                 <img src={icon} alt={title} className="w-12 h-12 mr-4" />
                 <h3 className="text-2xl font-russo text-white">{title}</h3>
--- src/components/sections/Tiles.tsx
+++ src/components/sections/Tiles.tsx
@@ -3,12 +3,12 @@ import { tiles } from "@/consts";
 
 const Tiles = () => {
     return (
-        <div className="bg-gradient-to-b from-gray-900 to-darkBlue animate-fadeInUp">
+        <div className="bg-gradient-to-b from-gray-900 to-darkBlue">
             <section className="container py-12 text-center mx-auto">
                 <main className="flex-grow">
                     <div className="font-jost flex flex-wrap custom-gap gap-2 items-center">
                         {tiles.map((tile, index) => (
-                            <div key={tile.id} className={`tile-hover-effect ${index >= tiles.length - (tiles.length % 3) ? 'justify-start' : 'justify-center'}`}>
+                            <div key={tile.id} className="tile-hover-effect fade-in-top" style={{ animationDelay: `${index * 0.2}s` }}>
                                 <Tile title={tile.title} description={tile.description} icon={tile.icon} />
                             </div>
                         ))}
