aecc2abddfb6cc78ba916af91e32b7cd38ab9495
Michal Witkowski <WitkowskiMichau@gmail.com>
2024-10-13 22:21:11 +0100

    work in progress #4

--- src/components/sections/Tiles.tsx
+++ src/components/sections/Tiles.tsx
@@ -6,9 +6,9 @@ const Tiles = () => {
         <div className='bg-darkBlue'>
             <section className="container py-12 text-center mx-auto">
                 <main className="flex-grow">
-                    <div className="font-jost flex flex-wrap custom-gap justify-center gap-2 items-center">
-                        {tiles.map((tile) => (
-                            <div key={tile.id} className="tile-hover-effect">
+                    <div className="font-jost flex flex-wrap custom-gap gap-2 items-center">
+                        {tiles.map((tile, index) => (
+                            <div key={tile.id} className={`tile-hover-effect ${index >= tiles.length - (tiles.length % 3) ? 'justify-start' : 'justify-center'}`}>
                                 <Tile title={tile.title} description={tile.description} icon={tile.icon} />
                             </div>
                         ))}
