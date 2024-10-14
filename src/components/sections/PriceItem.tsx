72f72d4d6422ca842f4d3cbfa6a837612b10154c
Michal Witkowski <WitkowskiMichau@gmail.com>
2024-10-14 19:27:41 +0100

    work in progress #8

--- src/components/sections/PriceItem.tsx
+++ src/components/sections/PriceItem.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const PriceItem = ({ title, price, features }) => {
+const PriceItem = ({ title, price, features, index }) => {
     return (
-        <div className="price-item-hover-effect p-6 bg-white rounded-lg shadow-md">
+        <div className="price-item-hover-effect p-6 bg-white rounded-lg shadow-md fade-in-top" style={{ animationDelay: `${index * 0.2}s` }}>
             <h3 className="text-2xl font-bold pb-2 mb-4 bg-gradient-to-r from-pink-500 to-blue-500 bg-clip-text text-transparent border-b-4 border-gradient-to-r">{title}</h3>
             <p className="text-xl mb-4">{price}</p>
             <ul className="list-disc list-inside">
