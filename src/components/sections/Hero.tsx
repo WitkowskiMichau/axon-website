7e8ba083469346c8f39fa73440a34561cde0e44d
Michal Witkowski <WitkowskiMichau@gmail.com>
2024-10-14 19:30:46 +0100

    fix lint errors

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,11 +3,11 @@ const Hero = () => {
         <div className="bg-gradient-to-b from-black to-gray-900 flex items-center justify-center">
             <section className="container text-center mx-auto pb-24">
                 <h2 className="pb-12 mt-60 text-5xl font-bold font-russo text-90sBlue animate-colorChange">
-                    Never miss your revenue quota using all world's largest LLMs.
+                    Never miss your revenue quota using all world&apos;s largest LLMs.
                 </h2>
                 <p className="text-2xl text-primaryYellow font-jost">
                     Upload your data or connect your data source and Axon will analyse, present <br />
-                    and find actionable plan for you. We don't use data externally in any shape or form.
+                    and find actionable plan for you. We don&apos;t use data externally in any shape or form.
                 </p>
             </section>
         </div>
--- src/components/sections/MovieSection.tsx
+++ src/components/sections/MovieSection.tsx
@@ -4,7 +4,7 @@ const MovieSection = () => {
             <section className="container py-12 text-center mx-auto">
                 <p className="text-3xl text-primaryYellow mb-6 font-russo fade-in-top">
                     Upload your data file or connect your CRM / User Data and Axon will help you <br/> understand what
-                    you're able to correct, enhance or prioritize.
+                    you&apos;re able to correct, enhance or prioritize.
                 </p>
                 <div className='py-8'>
                     <div className='mx-auto w-3/4 h-[500px] bg-black border-2 border-primaryYellow border-opacity-25 rounded-lg transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-102 hover:shadow-bright hover:border-shine cursor-pointer fade-in-top'>
