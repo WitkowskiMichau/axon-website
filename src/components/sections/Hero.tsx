b1ccc94ad0457190ca492c6926915341cd4efe2b
Michal Witkowski <WitkowskiMichau@gmail.com>
2024-11-11 13:58:03 +0000

    Hero $12312

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
         <div className="bg-gradient-to-b from-black to-gray-900 flex items-center justify-center">
             <section className="container mx-auto pb-24">
                 <h2 className="pb-12 mt-40 text-5xl font-bold font-russo text-90sBlue animate-colorChange text-center">
-                    Empower Your Business with Real-Time Data
+                    Use actionable data to run your business
                 </h2>
                 <ul className="text-2xl text-primaryYellow font-jost space-y-6 list-none flex flex-col items-center">
                     <li className="w-full text-center">Upload your data or connect to your preferred source effortlessly.</li>
